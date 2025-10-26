22471c7da48c954eecb3618c636d91b7e6f4ce36
ingfin14-spec <ingfin14@gmail.com>
2025-10-26 12:42:29 +0100

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,25 @@
-<meta name='viewport' content='width=device-width, initial-scale=1'/><script>// ==========================
+<meta name='viewport' content='width=device-width, initial-scale=1'/>// Firebase 100% working config for NeuroGallery
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-storage.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCK-o9vIFlrlySSyfamIf9bzL3BqNGIq3o",
+  authDomain: "neurogaller.firebaseapp.com",
+  projectId: "neurogaller",
+  storageBucket: "neurogaller.appspot.com",
+  messagingSenderId: "353726562637",
+  appId: "1:353726562637:web:8b2a53a1081b43a3cdb704",
+  measurementId: "G-2VW3RB5320"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();<script>// ==========================
 // 🔥 Firebase Config for NeuroGallery
 // ==========================
 
